43445622251c5085f0ac852fa8fc80b99e8cea5a
Kemran <109353270+remr2005@users.noreply.github.com>
2024-01-13 13:17:49 +0300

    Delete excel directory

--- excel/downloadFile.bat
+++
@@ -1,4 +0,0 @@
-cd c:\Users\USER\Desktop\yandexDisk_manipulation
-go run main
-move c:\Users\USER\Downloads\расписаниеЧЕТ.xlsx c:\Users\USER\Desktop\excel
-move c:\Users\USER\Downloads\расписаниеНЧЕТ.xlsx c:\Users\USER\Desktop\excel
